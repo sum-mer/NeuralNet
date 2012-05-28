448bb4e1cac4e3031dbd34250c5a91e21a2a07b1
Olga Zachariasz <olga.zachariasz@gmail.com>
2012-05-28 22:45:36 +0200

    zmiany w menu

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -4,15 +4,20 @@
  */
 package gui;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -24,14 +29,20 @@ import org.apache.commons.io.FileUtils;
  */
 public class MainWindow extends JFrame {
 
-    public JMenuBar jMenuBar;
-    public JMenu jMenu;
-    public JMenu removeImageJMenu;
-    public JMenuItem browseImagesJMenu;
-    public JMenuItem jMenuItem;
-    public JFileChooser fc;
-    public File folderName;
-    public File brandDirs[];
+    private JMenuBar jMenuBar;
+    private JMenu jMenu;
+    private JMenu removeImageJMenu;
+    private JMenuItem addBrandJMenuItem;
+    private JMenuItem browseImagesJMenu;
+    private JMenuItem jMenuItem;
+    private JFileChooser fc;
+    private File folderName;
+    private File brandDirs[];
+    private static BufferedImage img;
+    private ImageIcon iconOrg;
+    private JLabel labelForImgOrg;
+    private JButton startButton;
+    private JPanel panelForImg;
 
     public MainWindow(String car_Recognition) throws IOException {
         super(car_Recognition);
@@ -41,10 +52,59 @@ public class MainWindow extends JFrame {
 
         folderName = new File("images");
         brandDirs = folderName.listFiles();
+        startButton = new JButton("Start");
+        panelForImg = new JPanel();
+
+        labelForImgOrg = new JLabel();
+        labelForImgOrg.setHorizontalAlignment(JLabel.CENTER);
+        labelForImgOrg.setBorder(BorderFactory.createLineBorder(Color.black, 3));
+        panelForImg.setLayout(new FlowLayout(FlowLayout.CENTER));
+        panelForImg.add(labelForImgOrg);
+        panelForImg.add(startButton);
+        setCanvas();
 
         jMenuBar = new JMenuBar();
+        jMenu = new JMenu("Plik");
+        jMenu.setMnemonic(KeyEvent.VK_P);
+        jMenu.getAccessibleContext().setAccessibleDescription(
+                "Main menu with settings");
+        jMenuBar.add(jMenu);
+
+        browseImagesJMenu = new JMenuItem("Wybierz zdjęcie");
+        browseImagesJMenu.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                fc = new JFileChooser();
+                FileFilter filter = new FileNameExtensionFilter("Image files", "jpg", "jpeg", "png", "bmp");
+                fc.setAcceptAllFileFilterUsed(false);
+                fc.addChoosableFileFilter(filter);
+                int ret = fc.showDialog(null, "Wybierz plik do rozpoznania");
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    try {
+                        File fileToRecognize = fc.getSelectedFile();
+
+                        JOptionPane.showMessageDialog(null, "Wybrano plik: " + fileToRecognize.getName(),
+                                "Plik do rozpoznania", 1);
+
+                        img = ImageIO.read(fileToRecognize);
+
+                        iconOrg = new ImageIcon(getScaledImage(img, 256, 256));
+
+                        labelForImgOrg.setIcon(iconOrg);
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+
+            }
+        });
+
+        jMenu.add(browseImagesJMenu);
+
         jMenu = new JMenu("Baza");
-        jMenu.setMnemonic(KeyEvent.VK_A);
+        jMenu.setMnemonic(KeyEvent.VK_B);
         jMenu.getAccessibleContext().setAccessibleDescription(
                 "Main menu with settings");
         jMenuBar.add(jMenu);
@@ -61,7 +121,7 @@ public class MainWindow extends JFrame {
         });
 
         //jMenu.addSeparator();
-        removeImageJMenu = new JMenu("Dodaj zdjęcie");
+        removeImageJMenu = new JMenu("Dodaj zdjęcie do marki");
         removeImageJMenu.setMnemonic(KeyEvent.VK_S);
         removeImageJMenu.addActionListener(new ActionListener() {
 
@@ -71,18 +131,8 @@ public class MainWindow extends JFrame {
         createBrandListMenu();
 
 
-
-        browseImagesJMenu = new JMenuItem("Przeglądaj zdjęcia");
-        browseImagesJMenu.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent ae) {
-            }
-        });
-
-
-
-        jMenuItem = new JMenuItem("Dodaj nową ...");
-        jMenuItem.addActionListener(new ActionListener() {
+        addBrandJMenuItem = new JMenuItem("Dodaj nową markę");
+        addBrandJMenuItem.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent ae) {
                 String newBrandName = JOptionPane.showInputDialog(null, "Wpisz nazwę marki : ",
@@ -104,15 +154,34 @@ public class MainWindow extends JFrame {
 
             }
         });
-        removeImageJMenu.add(jMenuItem);
-        jMenu.add(browseImagesJMenu);
+        //removeImageJMenu.add(jMenuItem);
+        //jMenu.add(browseImagesJMenu);
         jMenu.add(removeImageJMenu);
+        jMenu.add(addBrandJMenuItem);
 
-        jMenu = new JMenu("Informacje");
+        jMenu = new JMenu("About");
 
         jMenu.getAccessibleContext().setAccessibleDescription(
                 "Help menu");
-        jMenuItem = new JMenuItem("Jakieś informacje");
+        jMenuItem = new JMenuItem("O programie");
+        jMenuItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Projekt z przedmiotu Sztuczne Sieci Neuronowe ",
+                        "O programie", 1);
+            }
+        });
+        jMenu.add(jMenuItem);
+
+        jMenuItem = new JMenuItem("Autorzy");
+        jMenuItem.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Aleksander Sobol & Olga Zachariasz",
+                        "Autorzy", 1);
+            }
+        });
+
         jMenu.add(jMenuItem);
         jMenuBar.add(jMenu);
 
@@ -120,7 +189,8 @@ public class MainWindow extends JFrame {
 
 
         //this.add(jMenuBar);
-        this.setLayout(null);
+        this.setLayout(new BorderLayout());
+        this.getContentPane().add(panelForImg, BorderLayout.CENTER);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setSize(600, 600);
@@ -128,40 +198,40 @@ public class MainWindow extends JFrame {
 
     private void createBrandListMenu() {
 
-        List<File> brandList =  Arrays.asList(brandDirs);  
+        List<File> brandList = Arrays.asList(brandDirs);
         Collections.sort(brandList);
         for (final File brandName : brandList) {
-            if (!brandName.getName().equals("test")) {
+            if (!brandName.getName().equals("test") && brandName.isDirectory()) {
                 if (notExist(brandName)) {
-                jMenuItem = new JMenuItem(brandName.getName());
-                jMenuItem.setName(brandName.getName());
-                jMenuItem.addActionListener(new ActionListener() {
+                    jMenuItem = new JMenuItem(brandName.getName());
+                    jMenuItem.setName(brandName.getName());
+                    jMenuItem.addActionListener(new ActionListener() {
 
-                    public void actionPerformed(ActionEvent ae) {
-                        fc = new JFileChooser();
-                        FileFilter filter = new FileNameExtensionFilter("Image files", "jpg", "jpeg", "png", "bmp");
-                        fc.setAcceptAllFileFilterUsed(false);
-                        fc.addChoosableFileFilter(filter);
-                        int ret = fc.showDialog(null, "Wybierz plik");
+                        public void actionPerformed(ActionEvent ae) {
+                            fc = new JFileChooser();
+                            FileFilter filter = new FileNameExtensionFilter("Image files", "jpg", "jpeg", "png", "bmp");
+                            fc.setAcceptAllFileFilterUsed(false);
+                            fc.addChoosableFileFilter(filter);
+                            int ret = fc.showDialog(null, "Wybierz plik");
 
-                        if (ret == JFileChooser.APPROVE_OPTION) {
-                            File file = fc.getSelectedFile();
-                            File source = file.getAbsoluteFile();
-                            File desc = new File(brandName.getAbsolutePath());
-                            try {
-                                FileUtils.copyFileToDirectory(source, desc);
-                                JOptionPane.showMessageDialog(null, "Dodano plik " + source.getName() + " do folderu marki " + brandName.getName(),
-                                        "Nowe zdjęcie", 1);
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                            if (ret == JFileChooser.APPROVE_OPTION) {
+                                File file = fc.getSelectedFile();
+                                File source = file.getAbsoluteFile();
+                                File desc = new File(brandName.getAbsolutePath());
+                                try {
+                                    FileUtils.copyFileToDirectory(source, desc);
+                                    JOptionPane.showMessageDialog(null, "Dodano plik " + source.getName() + " do folderu marki " + brandName.getName(),
+                                            "Nowe zdjęcie", 1);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
                             }
-
                         }
-                    }
-                });
-                removeImageJMenu.add(jMenuItem);
+                    });
+                    removeImageJMenu.add(jMenuItem);
                 }
-                
+
             }
         }
     }
@@ -171,8 +241,31 @@ public class MainWindow extends JFrame {
         for (int i = 0; i < count; i++) {
             if (f.getName().equals(removeImageJMenu.getItem(i).getName())) {
                 return false;
-            } 
+            }
         }
         return true;
+    }
+
+    private Image getScaledImage(Image srcImg, int w, int h) {
+        BufferedImage resizedImg = new BufferedImage(w, h,
+                BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(srcImg, 0, 0, w, h, null);
+        g2.dispose();
+        return resizedImg;
+    }
+
+    private void setCanvas() {
+        BufferedImage image;
+        try {
+            image = ImageIO.read(new File("images/canvas.jpg"));
+            ImageIcon canvas = new ImageIcon(getScaledImage(image, 256, 256));
+            labelForImgOrg.setIcon(canvas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
